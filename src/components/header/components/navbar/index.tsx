7eca383aec5644a1ea1df4f976d6d789ec14fc45
Cleidson Oliveira <dev.cleidson@gmail.com>
2022-04-15 19:40:13 -0300

    refactor: estilizando navbar

--- src/components/header/components/navbar/index.tsx
+++ src/components/header/components/navbar/index.tsx
@@ -1,15 +1,16 @@
+import { Link } from "react-router-dom";
 import { Container, Navlinks } from "./styles";
 
 export default function NavbarComponent() {
-  return ( <Container>
+  return (<Container>
     <div className="logo">
-                      <p>dinnerdash</p>
-                  </div>
-                      <Navlinks>
-                          <li>backoffice</li>
-                          <li>perfil</li>
-                          <li>sair</li>
-                      </Navlinks>
+      <p>dinnerdash</p>
+    </div>
+    <Navlinks>
+      <li><Link to="#">backoffice</Link></li>
+      <li><Link to="#">perfil</Link></li>
+      <li><Link to="#">sair</Link></li>
+    </Navlinks>
   </Container>
   );
 }
--- src/components/header/components/navbar/styles.ts
+++ src/components/header/components/navbar/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.nav`
     font-weight: bold;
-    width: 100%;
     color: var(--nav-links-color);
     display: flex;
     justify-content: space-between;
@@ -14,5 +13,12 @@ export const Navlinks = styled.ul `
     display: flex;
     list-style-type: none;
     max-width: 100%;
+    li:not(:first-child) {
+        padding-left: 10px;
+    }
 
+    li > a, a:visited {
+        text-decoration: none;
+        color: var(--nav-links-color);
+    }
 `
