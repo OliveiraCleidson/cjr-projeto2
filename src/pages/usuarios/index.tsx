4e4ab69783a08a711fb3700ca311748888710e24
Luis Fernando Lamellas <lflamellas@icloud.com>
2022-04-18 21:50:21 -0300

    feat: tabela da página de usuários criada e estilizada

--- src/pages/usuarios/index.tsx
+++ src/pages/usuarios/index.tsx
@@ -1,9 +1,47 @@
-import { Container } from "./styles";
+import { Container, TableButton, TableContainer, TableHeader, TableRow } from "./styles";
 
 export default function UsuariosPage() {
   return (
     <Container>
-      Teste
+      <TableContainer>
+        <TableHeader>
+          <div>
+            Nome
+          </div>
+          <div>
+            Email
+          </div>
+          <div>
+            Cadastrado em
+          </div>
+          <div>
+
+          </div>
+        </TableHeader>
+
+        <TableRow>
+          <div>
+            Lauren Shaxby
+          </div>
+          <div>
+            lshaxby@php.net
+          </div>
+          <div>
+            16/10/2021
+          </div>
+          <div className="buttonCell">
+            <TableButton variant='editar'>
+              editar
+            </TableButton>
+          </div>
+          <div className="buttonCell"> 
+            <TableButton variant='excluir'>
+              excluir
+            </TableButton>
+          </div>      
+        </TableRow>
+
+      </TableContainer>
     </Container>
   )
 }
--- src/pages/usuarios/styles.ts
+++ src/pages/usuarios/styles.ts
@@ -2,5 +2,47 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  background: red;
+`
+
+export const TableContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`
+
+export const TableHeader = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 2fr 2fr 1.4fr;
+  background: var(--table-head-bg);
+  padding: 0.5rem;
+  margin-top: 1rem;
+`
+
+export const TableRow = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 2fr 2fr 0.7fr 0.7fr;
+  padding: 0.5rem;
+
+  .buttonCell {
+    margin: 0 auto;
+  }
+`
+
+type TableButtonProps = {
+ variant: 'editar' | 'excluir' 
+}
+
+export const TableButton = styled.button<TableButtonProps>`
+  color: ${({variant}) => variant == 'editar' ? 'var(--edit-button-color)' : 'var(--delete-button-color)'};
+  border: none;
+  text-decoration: none;
+  background: none;
+  cursor: pointer;
+  transition-duration: 0.4s;
+  font-weight: bold;
+  
+  :hover {
+    color: var(--button-hover-color);
+  }
 `
